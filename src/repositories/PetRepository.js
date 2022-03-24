04b832cde5d1b9fc8855ece35e919b7ed92e0a8c
Racchel <racchelvelasco@gmail.com>
2022-03-24 14:35:19 -0300

    Add pet repository

---
+++ src/repositories/PetRepository.js
@@ -0,0 +1,30 @@
+import pool from '../config/db.js'
+
+export default class PetRepository {
+
+   constructor(){}
+
+   async list () {
+      return await pool.query('SELECT * FROM tb_pets')
+   }
+
+   async findByName(name) {
+      return await pool.query(`SELECT * FROM tb_pets WHERE name='${name}'`)
+   }
+
+   async create(name, age, image_url) {
+      return await pool.query(`INSERT INTO tb_pets (name, age, image_url) VALUES ('${name}', '${age}', '${image_url}') RETURNING *`)
+   }
+
+   async update(id, name, age, image_url) {
+      return await pool.query(`UPDATE tb_pets SET name='${name}', age='${age}', image_url='${image_url}' WHERE pet_id=${id} RETURNING *`)
+   }
+
+   async updateImage(id, image_url) {
+      return await pool.query(`UPDATE tb_pets SET image_url='${image_url}' WHERE pet_id=${id} RETURNING *`)
+   }
+
+   async delete(id) {
+      return await pool.query(`DELETE FROM tb_pets WHERE pet_id=${id}`)
+   }
+}
---
+++ src/repositories/index.js
@@ -0,0 +1,5 @@
+import PetRepository from './PetRepository.js'
+
+export {
+   PetRepository
+}
