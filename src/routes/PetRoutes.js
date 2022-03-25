d50830849bbe0c582991944705709427674c1fb9
Racchel <racchelvelasco@gmail.com>
2022-03-25 13:14:05 -0300

    Use middleware on routes that take the ID as a parameter

--- src/routes/PetRoutes.js
+++ src/routes/PetRoutes.js
@@ -1,18 +1,29 @@
 import { Router } from 'express'
 
+import petExistsById from '../middlewares/petExistsById.js'
+
 import { PetController } from '../controllers/index.js'
 const petController = new PetController()
 
 const petRoutes = Router()
 
+
 // Lista todos os pets
 petRoutes.get('/', petController.list)
 
 // Lista pet por nome
-petRoutes.get('/:name', petController.findByName)
+petRoutes.get('/findByName/:name', petController.findByName)
 
 // Cria um novo pet
 petRoutes.post('/', petController.create)
+
+/***/
+
+// Middleware para validar se o pet existe
+petRoutes.use('/:id', petExistsById)
+
+// Lista pet por id
+petRoutes.get('/:id', petController.findById)
 
 // Atualiza os dados que forem enviados
 petRoutes.put('/:id', petController.update)
