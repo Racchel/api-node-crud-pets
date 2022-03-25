e3180d2cccffa068214400c0f0e2490a52870d01
Racchel <racchelvelasco@gmail.com>
2022-03-25 13:12:16 -0300

    Use bind in constructor methods and add findByName method validation

--- src/controllers/PetController.js
+++ src/controllers/PetController.js
@@ -1,17 +1,25 @@
 import { PetRepository } from '../repositories/index.js'
-const petRepository = new PetRepository()
 
 export default class PetController {
 
-   constructor(){}
+   constructor(){
+      this.petRepository = new PetRepository()
+      this.list = this.list.bind(this)
+      this.findById = this.findById.bind(this)
+      this.findByName = this.findByName.bind(this)
+      this.create = this.create.bind(this)
+      this.update = this.update.bind(this)
+      this.updateImage = this.updateImage.bind(this)
+      this.delete = this.delete.bind(this)
+   }
 
    async list (req, res) {
 
       try {
-         const petList = await petRepository.list()
+         const petList = await this.petRepository.list()
          return res.status(200).send(petList.rows)
       } catch (err) {
-         console.log(err)
+         console.error(err)
          return res.status(400).send(err)
       }
    }
@@ -20,10 +28,29 @@ export default class PetController {
       const { name } = req.params
 
       try {
-         const petByName = await petRepository.findByName(name)
+         const petByName = await this.petRepository.findByName(name)
+
+         if (petByName.rows.length === 0) {
+            return res.status(404).send({
+               message: `Pet '${name}' does not exist`
+            })
+         }
+
          return res.status(200).send(petByName.rows)
       } catch (err) {
-         console.log(err)
+         console.error(err)
+         return res.status(400).send(err)
+      }
+   }
+
+   async findById(req, res) {
+      const { id } = req.params
+
+      try {
+         const petByName = await this.petRepository.findById(id)
+         return res.status(200).send(petByName.rows)
+      } catch (err) {
+         console.error(err)
          return res.status(400).send(err)
       }
    }
@@ -32,10 +59,10 @@ export default class PetController {
       const { name, age, image_url } = req.body
 
       try {
-         const newPet = await petRepository.create(name, age, image_url)
+         const newPet = await this.petRepository.create(name, age, image_url)
          return res.status(200).send(newPet.rows)
       } catch (err) {
-         console.log(err)
+         console.error(err)
          return res.status(400).send(err)
       }
    }
@@ -45,10 +72,10 @@ export default class PetController {
       const { name, age, image_url } = req.body
 
       try {
-         const updatedPet = await petRepository.update(id, name, age, image_url)
+         const updatedPet = await this.petRepository.update(id, name, age, image_url)
          return res.status(200).send(updatedPet.rows)
       } catch (err) {
-         console.log(err)
+         console.error(err)
          return res.status(400).send(err)
       }
    }
@@ -58,10 +85,10 @@ export default class PetController {
       const { image_url } = req.body
 
       try {
-         const newPet = await petRepository.updateImage(id, image_url)
+         const newPet = await this.petRepository.updateImage(id, image_url)
          return res.status(200).send(newPet.rows)
       } catch (err) {
-         console.log(err)
+         console.error(err)
          return res.status(400).send(err)
       }
    }
@@ -70,10 +97,13 @@ export default class PetController {
       const { id } = req.params
 
       try {
-         const deletedPet = await petRepository.delete(id)
-         return res.status(200).send(deletedPet.rows)
+         const deletedPet = await this.petRepository.delete(id)
+         return res.status(200).json({
+            message: 'Pet successfully deleted',
+            deletedPet: deletedPet.rows
+         })
       } catch (err) {
-         console.log(err)
+         console.error(err)
          return res.status(400).send(err)
       }
    }
